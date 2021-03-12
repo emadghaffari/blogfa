05fb4150e8cc107c373258145f8f7ba22c73afa5
Emad <emadghaffariii@gmail.com>
2021-03-12 15:17:54 +0000

    cleanup

--- auth/model/user/user_dao.go
+++ auth/model/user/user_dao.go
@@ -1,6 +1,9 @@
 package user
 
-import "time"
+import (
+	"blogfa/auth/model/role"
+	"time"
+)
 
 // User model
 type User struct {
@@ -14,25 +17,7 @@ type User struct {
 	BirthDate string    `json:"birthDate" gorm:"type:varchar(50);"`
 	Gender    string    `json:"gender" gorm:"type:varchar(20);"`
 	RoleID    uint64    `json:"-"`
-	Role      Role      `json:"role" gorm:"foreignKey:RoleID;references:ID"`
-	CreatedAt time.Time `json:"-"`
-	UpdatedAt time.Time `json:"-"`
-}
-
-// Role struct
-type Role struct {
-	ID          uint64        `json:"-" gorm:"primaryKey"`
-	Name        string        `json:"name" gorm:"unique;not null;type:varchar(30);"`
-	Permissions []*Permission `json:"permissions" gorm:"many2many:roles_permissions;association_foreignkey:ID;foreignkey:ID"`
-	CreatedAt   time.Time     `json:"-"`
-	UpdatedAt   time.Time     `json:"-"`
-}
-
-// Permission struct
-type Permission struct {
-	ID        uint64    `json:"-" gorm:"primaryKey"`
-	Name      string    `json:"name" gorm:"unique;not null;type:varchar(30);"`
-	Role      []*Role   `json:"-" gorm:"many2many:roles_permissions;association_foreignkey:ID;foreignkey:ID"`
+	Role      role.Role `json:"role" gorm:"foreignKey:RoleID;references:ID"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
