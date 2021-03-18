c0eff705ee8f50e44487fa1f6bb68b2da11c7f3a
Emad <emadghaffariii@gmail.com>
2021-03-18 19:40:49 +0000

    refactor model

--- auth/model/user/user_dto.go
+++ auth/model/user/user_dto.go
@@ -1,7 +1,14 @@
 package user
 
-func (u *User) Store() {
+import (
+	"blogfa/auth/database/mysql"
+	"context"
+)
 
+func (u *User) Register(ctx context.Context) error {
+	mysql.Storage.Create(ctx, u)
+
+	return nil
 }
 
 func (u *User) Get() {
