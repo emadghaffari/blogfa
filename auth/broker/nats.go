a1a850d917330280ecc5eeb6dd7db2acc2e395ee
Emad <emadghaffariii@gmail.com>
2021-03-21 13:25:47 +0000

    [init] nats broker with connection methods

--- auth/broker/nats.go
+++ auth/broker/nats.go
@@ -1,16 +1,48 @@
 package broker
 
-import "sync"
+import (
+	"blogfa/auth/config"
+	"strings"
+	"sync"
+	"time"
+
+	nats "github.com/nats-io/nats.go"
+)
 
 var (
-	Nats NatsBroker = &nats{}
-	nts  sync.Once
+	Nats  NatsBroker = &nts{}
+	oncen sync.Once
 )
 
 type NatsBroker interface{}
 
-type nats struct{}
+type nts struct {
+	conn *nats.Conn
+}
 
-func (n *nats) Connect() {
-	nts.Do(func() {})
+func (n *nts) Connect() error {
+	var err error
+	oncen.Do(func() {
+		opts := nats.Options{
+			Name:         config.Global.Service.Name,
+			Secure:       config.Global.Nats.Auth,
+			User:         config.Global.Nats.Username,
+			Password:     config.Global.Nats.Password,
+			MaxReconnect: 10,
+			Url:          strings.Join(config.Global.Nats.Endpoints, ","),
+			PingInterval: time.Minute * 10,
+		}
+
+		n.conn, err = opts.Connect()
+		if err != nil {
+			return
+		}
+
+	})
+
+	return err
+}
+
+func (n *nts) GetConnection() *nats.Conn {
+	return n.conn
 }
