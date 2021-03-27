e1f59538c07a27b80171db2628de40c9e18a2b68
Emad <emadghaffariii@gmail.com>
2021-03-27 15:36:57 +0000

    basic span

--- auth/model/user/user_dto.go
+++ auth/model/user/user_dto.go
@@ -9,6 +9,7 @@ func (u *User) Register(ctx context.Context, user User) error {
 	// mysql.Storage.Create(ctx, u)
 	span, _ := jtrace.Tracer.SpanFromContext(ctx, "Register_user")
 	defer span.Finish()
+	span.SetTag("register", "register user model")
 
 	return nil
 }
