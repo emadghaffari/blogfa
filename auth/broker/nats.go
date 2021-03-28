42a16202a5e6d95b65bea9a4da1b58f77546032b
Emad <emadghaffariii@gmail.com>
2021-03-28 19:36:57 +0000

    add ec encode connection and publish method

--- auth/broker/nats.go
+++ auth/broker/nats.go
@@ -2,11 +2,15 @@ package broker
 
 import (
 	"blogfa/auth/config"
+	zapLogger "blogfa/auth/pkg/logger"
+	"context"
+	"fmt"
 	"strings"
 	"sync"
 	"time"
 
 	nats "github.com/nats-io/nats.go"
+	"go.uber.org/zap"
 )
 
 var (
@@ -14,10 +18,14 @@ var (
 	oncen sync.Once
 )
 
-type NatsBroker interface{}
+type NatsBroker interface {
+	Connect() error
+	Conn() *nats.Conn
+}
 
 type nts struct {
 	conn *nats.Conn
+	ec   *nats.EncodedConn
 }
 
 func (n *nts) Connect() error {
@@ -35,6 +43,11 @@ func (n *nts) Connect() error {
 
 		n.conn, err = opts.Connect()
 		if err != nil {
+			logger := zapLogger.GetZapLogger(false)
+			zapLogger.Prepare(logger).
+				Development().
+				Level(zap.ErrorLevel).
+				Commit(err.Error())
 			return
 		}
 
@@ -43,6 +56,40 @@ func (n *nts) Connect() error {
 	return err
 }
 
+func (n *nts) EncodedConn() error {
+	if n.conn == nil {
+		logger := zapLogger.GetZapLogger(false)
+		zapLogger.Prepare(logger).
+			Development().
+			Level(zap.ErrorLevel).
+			Commit("ERROR")
+		return fmt.Errorf("nats not connected, connect first")
+	}
+
+	var err error
+	n.ec, err = nats.NewEncodedConn(n.conn, nats.JSON_ENCODER)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (n *nts) Conn() *nats.Conn {
 	return n.conn
+}
+func (n *nts) ECConn() *nats.EncodedConn {
+	return n.ec
+}
+
+func (n *nts) Publish(ctx context.Context, subject string, value interface{}) error {
+	if err := n.ec.Publish(subject, &value); err != nil {
+		logger := zapLogger.GetZapLogger(false)
+		zapLogger.Prepare(logger).
+			Development().
+			Level(zap.ErrorLevel).
+			Commit(err.Error())
+		return err
+	}
+
+	return nil
 }
