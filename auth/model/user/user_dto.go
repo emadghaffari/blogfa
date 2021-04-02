326eee76db12db261c3f5276ba9711c026134bf1
Emad <emadghaffariii@gmail.com>
2021-04-02 13:18:40 +0000

    add table handler for store user

--- auth/model/user/user_dto.go
+++ auth/model/user/user_dto.go
@@ -1,15 +1,19 @@
 package user
 
 import (
+	"blogfa/auth/database/mysql"
 	"blogfa/auth/pkg/jtrace"
 	"context"
 )
 
 func (u *User) Register(ctx context.Context, user User) error {
-	// mysql.Storage.Create(ctx, u)
 	span, _ := jtrace.Tracer.SpanFromContext(ctx, "Register_user")
 	defer span.Finish()
 	span.SetTag("register", "register user model")
+
+	if err := mysql.Storage.Create(jtrace.Tracer.ContextWithSpan(ctx, span), "users", user); err != nil {
+		return err
+	}
 
 	return nil
 }
