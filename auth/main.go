426258392cfb331dece671fe04db1fd7788e0ef6
Emad <emadghaffariii@gmail.com>
2021-04-03 17:50:14 +0000

    comment

--- auth/main.go
+++ auth/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 )
 
+// root execute command with cobra
 func main() {
 	if err := cmd.RootCmd().Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to run command: %v\n", err)
