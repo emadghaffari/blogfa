ffaca71c3bbb514e10ece4c7eda343ff297e831f
Emad <emadghaffariii@gmail.com>
2021-04-06 18:06:13 +0000

    add sql attrs for store provider

--- auth/model/provider/provider_dto.go
+++ auth/model/provider/provider_dto.go
@@ -1,14 +1,23 @@
 package provider
 
 import (
+	"blogfa/auth/database/mysql"
 	"blogfa/auth/pkg/jtrace"
 	"context"
 )
 
-func (p *Provider) Register(ctx context.Context, user Provider) error {
+func (p *Provider) Register(ctx context.Context, prov Provider) error {
 	span, _ := jtrace.Tracer.SpanFromContext(ctx, "register_provider")
 	defer span.Finish()
 	span.SetTag("register", "register provider model")
+
+	tx := mysql.Storage.GetDatabase().Begin()
+
+	if err := tx.Create(&prov).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+	defer tx.Commit()
 
 	return nil
 }
