d7c56738a1bc24d8108e739294f92f9a859daedc
Emad <emadghaffariii@gmail.com>
2021-04-08 19:36:21 +0000

    add username and password login

--- auth/service/up-login.go
+++ auth/service/up-login.go
@@ -1,14 +1,40 @@
 package service
 
 import (
+	"blogfa/auth/model/user"
+	"blogfa/auth/pkg/cript"
 	"blogfa/auth/pkg/jtrace"
 	pb "blogfa/auth/proto"
 	"context"
+	"fmt"
 )
 
 func (a *Auth) UPLogin(ctx context.Context, req *pb.UPLoginRequest) (*pb.UPLoginResponse, error) {
 	span := jtrace.Tracer.StartSpan("up-login")
 	defer span.Finish()
+	span.SetTag("register", "username password login")
 
-	return &pb.UPLoginResponse{}, nil
+	password, err := cript.Hash(req.GetPassword())
+	if err != nil {
+		return &pb.UPLoginResponse{Message: fmt.Sprintf("ERROR: %s", err.Error()), Status: &pb.Response{Code: 400, Message: "FAILED"}}, fmt.Errorf("error in hash password: %s", err.Error())
+	}
+
+	user, err := user.Model.Get(jtrace.Tracer.ContextWithSpan(ctx, span), "users", req.GetUsername())
+	if err != nil || user.Password != &password {
+		return &pb.UPLoginResponse{
+			Message: "username or password not matched! ",
+			Status: &pb.Response{
+				Code:    403,
+				Message: "invalid username or password",
+			},
+		}, fmt.Errorf("invalid username or password")
+	}
+
+	return &pb.UPLoginResponse{
+		Message: "user loggedin successfully",
+		Status: &pb.Response{
+			Code:    200,
+			Message: "user loggedin successfully",
+		},
+	}, nil
 }
