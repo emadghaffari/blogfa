ce33649b08c1204243500dd14358e2e286c32e5d
Emad <emadghaffariii@gmail.com>
2021-04-09 15:43:34 +0000

    add redis configs

--- auth/config/redis.go
+++ auth/config/redis.go
@@ -2,5 +2,9 @@ package config
 
 // redis struct
 type redis struct {
-	Address string `json:"address" yaml:"redis.address"`
+	Username string `yaml:"redis.username"`
+	Password string `yaml:"redis.password"`
+	DB       int    `yaml:"redis.db"`
+	Host     string `yaml:"redis.host"`
+	Logger   bool   `yaml:"redis.logger"`
 }
