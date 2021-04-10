0174ec684d7a80c6328ccd62e12096a5acd91e04
Emad <emadghaffariii@gmail.com>
2021-04-10 18:30:47 +0000

    add ToList func

--- auth/model/permission/permission_dto.go
+++ auth/model/permission/permission_dto.go
@@ -1,1 +1,10 @@
 package permission
+
+func ToList(Permissions []*Permission) []string {
+	response := make([]string, len(Permissions))
+	for _, v := range Permissions {
+		response = append(response, v.Name)
+	}
+
+	return response
+}
