d2a502f03c72dd4dab4906041e211a0006ce0b26
Emad <emadghaffariii@gmail.com>
2021-04-11 15:45:16 +0000

    comment

--- auth/model/jwt/jwt_dao.go
+++ auth/model/jwt/jwt_dao.go
@@ -1,16 +1,8 @@
 package jwt
 
 import (
-	"blogfa/auth/config"
-	"blogfa/auth/database/redis"
-	zapLogger "blogfa/auth/pkg/logger"
-	"blogfa/auth/pkg/token"
 	"context"
-	"encoding/json"
-	"fmt"
-	"time"
 
-	jjwt "github.com/dgrijalva/jwt-go"
 	"go.uber.org/zap"
 )
 
@@ -37,104 +29,4 @@ type jwt struct {
 	RefreshUUID  string `json:"rau"`
 	AtExpires    int64  `json:"exp"`
 	RtExpires    int64  `json:"rexp"`
-}
-
-// Generate new jwt token and store into redis DB
-func (j *jwt) Generate(ctx context.Context, model interface{}) (*jwt, error) {
-	logger = zapLogger.GetZapLogger(false)
-
-	td, err := j.GenerateJWT()
-	if err != nil {
-		return nil, err
-	}
-
-	if err := j.genRefJWT(td); err != nil {
-		return nil, err
-	}
-
-	if err := j.store(ctx, model, td); err != nil {
-		return nil, err
-	}
-
-	return td, nil
-}
-
-// generate JWT tokens
-func (j *jwt) GenerateJWT() (*jwt, error) {
-	// create new jwt
-	td := &jwt{}
-	td.AtExpires = time.Now().Add(time.Duration(config.Global.Redis.UserDuration)).Unix()
-	td.RtExpires = time.Now().Add(time.Duration(config.Global.Redis.UserDuration)).Unix()
-	td.AccessUUID = token.Generate(30)
-	td.RefreshUUID = token.Generate(60)
-
-	// New MapClaims for access token
-	atClaims := jjwt.MapClaims{}
-	atClaims["authorized"] = true
-	atClaims["uuid"] = td.AccessUUID
-	atClaims["exp"] = td.AtExpires
-	at := jjwt.NewWithClaims(jjwt.SigningMethodHS256, atClaims)
-
-	var err error
-	td.AccessToken, err = at.SignedString([]byte(config.Global.JWT.Secret))
-	if err != nil {
-		zapLogger.Prepare(logger).
-			Development().
-			Level(zap.ErrorLevel).
-			Commit(err.Error())
-		return nil, err
-	}
-	return td, nil
-}
-
-// generate refresh tokens
-func (j *jwt) genRefJWT(td *jwt) error {
-	// New MapClaims for refresh access token
-	rtClaims := jjwt.MapClaims{}
-	rtClaims["uuid"] = td.RefreshUUID
-	rtClaims["exp"] = td.RtExpires
-	rt := jjwt.NewWithClaims(jjwt.SigningMethodHS256, rtClaims)
-
-	var err error
-	td.RefreshToken, err = rt.SignedString([]byte(config.Global.JWT.RSecret))
-	if err != nil {
-		zapLogger.Prepare(logger).
-			Development().
-			Level(zap.ErrorLevel).
-			Commit(err.Error())
-		return err
-	}
-	return nil
-}
-
-// store into DB
-func (j *jwt) store(ctx context.Context, model interface{}, td *jwt) error {
-	bt, err := json.Marshal(model)
-	if err != nil {
-		zapLogger.Prepare(logger).
-			Development().
-			Level(zap.ErrorLevel).
-			Commit(err.Error())
-		return fmt.Errorf("can not marshal data: %s", model)
-	}
-	at := time.Unix(td.AtExpires, 0) //converting Unix to UTC(to Time object)
-	now := time.Now()
-
-	// make map for store in redis
-	if err := redis.Storage.Set(ctx, td.AccessUUID, string(bt), at.Sub(now)); err != nil {
-		zapLogger.Prepare(logger).
-			Development().
-			Level(zap.ErrorLevel).
-			Commit(err.Error())
-		return err
-	}
-	return nil
-}
-
-// Get jwt token from redis
-func (j *jwt) Get(ctx context.Context, token string, response interface{}) error {
-	if err := redis.Storage.Get(ctx, token, &response); err != nil {
-		return err
-	}
-	return nil
 }
