e69799261711a69ca4a78d989e72171cc64c617d
Emad <emadghaffariii@gmail.com>
2021-04-13 17:16:08 +0000

    add assign provider to user

---
+++ auth/service/assignPU.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	pb "blogfa/auth/proto"
+	"context"
+)
+
+
+func (a *Auth) AssignProviderToUser(ctx context.Context, req *pb.AssignProviderToUserRequest) (*pb.Response, error)  {
+	
+}
