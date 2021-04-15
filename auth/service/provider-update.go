f31dcc5faa836b5d4f9cc3baae2e74dfc42fa0c8
Emad <emadghaffariii@gmail.com>
2021-04-15 13:42:23 +0000

    comment

--- auth/service/provider-update.go
+++ auth/service/provider-update.go
@@ -5,7 +5,7 @@ import (
 	"context"
 )
 
-
-func (a *Auth) UpdateProvider(ctx context.Context, req *pb.UpdateProviderRequest) (*pb.Response, error)  {
-	return &pb.Response{},nil
+// UpdateProvider method for update provider
+func (a *Auth) UpdateProvider(ctx context.Context, req *pb.UpdateProviderRequest) (*pb.Response, error) {
+	return &pb.Response{}, nil
 }
