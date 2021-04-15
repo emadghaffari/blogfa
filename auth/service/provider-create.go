1eb52ebee771d3b8b87778b254228c179164adc3
Emad <emadghaffariii@gmail.com>
2021-04-15 13:42:11 +0000

    comment

--- auth/service/provider-create.go
+++ auth/service/provider-create.go
@@ -5,7 +5,7 @@ import (
 	"context"
 )
 
-
-func (a *Auth) CreateProvider(ctx context.Context, req *pb.CreateProviderRequest) (*pb.Response, error)  {
-	return &pb.Response{},nil
+// CreateProvider method for create provider and assign to user
+func (a *Auth) CreateProvider(ctx context.Context, req *pb.CreateProviderRequest) (*pb.Response, error) {
+	return &pb.Response{}, nil
 }
