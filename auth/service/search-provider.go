c4c12ad5387b67929e8535a986270033584839fc
Emad <emadghaffariii@gmail.com>
2021-04-15 13:42:34 +0000

    comment

--- auth/service/search-provider.go
+++ auth/service/search-provider.go
@@ -2,7 +2,7 @@ package service
 
 import pb "blogfa/auth/proto"
 
-
-func (a *Auth) SearchProvider(req *pb.SearchRequest, stream pb.Auth_SearchProviderServer) error  {
+// SearchProvider method for search user by provider fields
+func (a *Auth) SearchProvider(req *pb.SearchRequest, stream pb.Auth_SearchProviderServer) error {
 	return nil
 }
