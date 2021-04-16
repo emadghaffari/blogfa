e6d6654eff1d326199829defaa6a437d4c0e65f8
Emad <emadghaffariii@gmail.com>
2021-04-16 11:17:49 +0000

    update user

--- auth/model/user/user_dto.go
+++ auth/model/user/user_dto.go
@@ -3,7 +3,11 @@ package user
 import (
 	"blogfa/auth/database/mysql"
 	"blogfa/auth/pkg/jtrace"
+	"blogfa/auth/pkg/logger"
 	"context"
+	"fmt"
+
+	"go.uber.org/zap"
 )
 
 // Register method for register new user
@@ -15,6 +19,12 @@ func (u *User) Register(ctx context.Context, user User) (*User, error) {
 	tx := mysql.Storage.GetDatabase().Begin()
 
 	if err := tx.Create(&user).Error; err != nil {
+		log := logger.GetZapLogger(false)
+		logger.Prepare(log).
+			Append(zap.Any("error", fmt.Sprintf("register user: %s", err))).
+			Level(zap.ErrorLevel).
+			Development().
+			Commit("env")
 		tx.Rollback()
 		return nil, err
 	}
@@ -27,14 +37,20 @@ func (u *User) Register(ctx context.Context, user User) (*User, error) {
 func (u *User) Get(ctx context.Context, table string, query interface{}, args ...interface{}) (*User, error) {
 	span, _ := jtrace.Tracer.SpanFromContext(ctx, "get user model")
 	defer span.Finish()
-	span.SetTag("get", "get user model")
+	span.SetTag("model", "get user model")
 
 	tx := mysql.Storage.GetDatabase().Begin()
 
 	var user = User{}
-	if err := tx.Preload("Role").Preload("Role.Permissions").Table(table).Where(query, args...).First(&user); err.Error != nil {
+	if err := tx.Preload("Role").Preload("Role.Permissions").Table(table).Where(query, args...).First(&user).Error; err != nil {
+		log := logger.GetZapLogger(false)
+		logger.Prepare(log).
+			Append(zap.Any("error", fmt.Sprintf("get user: %s", err))).
+			Level(zap.ErrorLevel).
+			Development().
+			Commit("env")
 		tx.Rollback()
-		return nil, err.Error
+		return nil, err
 	}
 	defer tx.Commit()
 
@@ -42,6 +58,22 @@ func (u *User) Get(ctx context.Context, table string, query interface{}, args ..
 }
 
 func (u User) Update(ctx context.Context, user User) error {
+	span, _ := jtrace.Tracer.SpanFromContext(ctx, "get user model")
+	defer span.Finish()
+	span.SetTag("model", fmt.Sprintf("update user with id: %d", user.ID))
+
+	tx := mysql.Storage.GetDatabase().Begin()
+
+	if err := tx.Table("users").Where("id = ?", user.ID).Select("*").Updates(user).Error; err != nil {
+		log := logger.GetZapLogger(false)
+		logger.Prepare(log).
+			Append(zap.Any("error", fmt.Sprintf("update user error: %s", err))).
+			Level(zap.ErrorLevel).
+			Development().
+			Commit("env")
+		tx.Rollback()
+		return err
+	}
 
 	return nil
 }
