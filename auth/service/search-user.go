3d4e52b35e2594f00c9cc86b73efd4c6031e910c
Emad <emadghaffariii@gmail.com>
2021-04-17 19:04:43 +0000

    add search for users

--- auth/service/search-user.go
+++ auth/service/search-user.go
@@ -1,8 +1,23 @@
 package service
 
-import pb "blogfa/auth/proto"
+import (
+	"blogfa/auth/model/jwt"
+	"blogfa/auth/pkg/jtrace"
+	pb "blogfa/auth/proto"
+	"fmt"
+)
 
 // SearchUser method for search user by users fields
 func (a *Auth) SearchUser(req *pb.SearchRequest, stream pb.Auth_SearchUserServer) error {
+
+	span := jtrace.Tracer.StartSpan("search-user")
+	defer span.Finish()
+	span.SetTag("service", "start to search")
+
+	// verify the jwt token
+	if _, err := jwt.Model.Verify(req.GetToken()); err != nil {
+		return fmt.Errorf("user not verified: %s", err.Error())
+	}
+
 	return nil
 }
