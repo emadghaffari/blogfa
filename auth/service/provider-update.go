9a9460f113abacbc6769efdeadd2ba3d2c62adac
Emad <emadghaffariii@gmail.com>
2021-04-20 18:44:05 +0000

    add update method

--- auth/service/provider-update.go
+++ auth/service/provider-update.go
@@ -2,14 +2,17 @@ package service
 
 import (
 	"blogfa/auth/model/jwt"
+	"blogfa/auth/model/provider"
 	"blogfa/auth/pkg/jtrace"
 	pb "blogfa/auth/proto"
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
 )
 
 // UpdateProvider method for update provider
@@ -29,6 +32,60 @@ func (a *Auth) UpdateProvider(ctx context.Context, req *pb.UpdateProviderRequest
 		}, status.Errorf(codes.Internal, "user not verified")
 	}
 
-	// response
-	return &pb.Response{}, nil
+	// convert string to int for userID
+	userID, err := strconv.Atoi(req.GetUserID())
+	if err != nil {
+		return &pb.Response{
+			Message: fmt.Sprintf("invalid userID: %s", err.Error()),
+			Status: &pb.Status{
+				Code:    http.StatusUnauthorized,
+				Message: "FAILED",
+			},
+		}, status.Errorf(codes.Internal, "invalid userID")
+	}
+
+	// convert string to int for userID
+	ID, err := strconv.Atoi(req.GetID())
+	if err != nil {
+		return &pb.Response{
+			Message: fmt.Sprintf("invalid userID: %s", err.Error()),
+			Status: &pb.Status{
+				Code:    http.StatusUnauthorized,
+				Message: "FAILED",
+			},
+		}, status.Errorf(codes.Internal, "invalid userID")
+	}
+
+	// try to update provider
+	if err := provider.Model.Update(
+		jtrace.Tracer.ContextWithSpan(ctx, span),
+		provider.Provider{
+			Model:       gorm.Model{ID: uint(ID)},
+			UserID:      uint(userID),
+			FixedNumber: req.GetFixedNumber(),
+			Company:     req.GetCompany(),
+			CardNumber:  req.GetCardNumber(),
+			ShebaNumber: req.GetShebaNumber(),
+			Card:        req.GetCard(),
+			Address:     req.GetAddress(),
+		},
+	); err != nil {
+		return &pb.Response{
+			Message: "provider not updated successfully",
+			Status: &pb.Status{
+				Code:    http.StatusInternalServerError,
+				Message: "FAILED",
+			},
+		}, status.Errorf(codes.Internal, "provider not updated successfully")
+	}
+
+	// response if updated successfully
+	return &pb.Response{
+		Message: "provider successfully updated",
+		Status: &pb.Status{
+			Code:    http.StatusOK,
+			Message: "SUCCESS",
+		},
+	}, nil
+
 }
