065257f679aec3f6238f0934a5a571787ef723e0
Emad <emadghaffariii@gmail.com>
2021-04-30 19:12:30 +0000

    add user model

--- auth/model/permission/permission_dao.go
+++
@@ -1,9 +0,0 @@
-package permission
-
-import "gorm.io/gorm"
-
-// Permission struct
-type Permission struct {
-	gorm.Model
-	Name string `json:"name" gorm:"unique;not null;type:varchar(30);"`
-}
---
+++ auth/model/user.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+// User model
+type User struct {
+	gorm.Model
+	Username  string      `json:"username" validate:"required" gorm:"unique;not null;type:varchar(100);"`
+	Password  *string     `json:"-" validate:"required,gte=7" gorm:"type:varchar(100);"`
+	Name      string      `json:"name" validate:"required" gorm:"type:varchar(100);"`
+	LastName  string      `json:"lastName" validate:"required" gorm:"type:varchar(100);"`
+	Phone     string      `json:"phone" validate:"required" gorm:"type:varchar(25);"`
+	Email     string      `json:"email" gorm:"type:varchar(100);"`
+	BirthDate string      `json:"birthDate" gorm:"type:varchar(50);"`
+	Gender    string      `json:"gender" gorm:"type:varchar(20);"`
+	RoleID    uint64      `json:"-"`
+	Role      Role        `json:"role" gorm:"foreignKey:RoleID;references:ID"`
+	Provider  []*Provider `json:"provides" gorm:"foreignKey:UserID;references:ID"`
+}
