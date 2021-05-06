23d35e09afa8811dcffd52f4eb6cc4bfc6c0315a
Emad <emadghaffariii@gmail.com>
2021-05-06 16:59:58 +0000

    refactor and cleanup

--- auth/service/grpc/user-search.go
+++ auth/service/grpc/user-search.go
@@ -44,14 +44,8 @@ func (a *Auth) SearchUser(req *pb.SearchRequest, stream pb.Auth_SearchUserServer
 		return status.Errorf(codes.Internal, "internal error for search users")
 	}
 
-	// @FIXME
 	// provider list need to change
-	// bug for list providers for user
 	for _, user := range users {
-		fmt.Println("-------------------------")
-		fmt.Println(user.ID)
-		fmt.Println("-------------------------")
-
 		err := stream.Send(&pb.User{
 			Username:  user.Username,
 			Gender:    pb.User_Gender(pb.User_Gender_value[user.Gender]),
