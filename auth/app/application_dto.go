b76e413eaa0dc8156068b34a29fbfccfe5075aa6
Emad <emadghaffariii@gmail.com>
2021-05-06 18:41:35 +0000

    add middleware

--- auth/app/application_dto.go
+++ auth/app/application_dto.go
@@ -1,6 +1,11 @@
 package app
 
 import (
+	"blogfa/auth/service/middleware"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
 	group "github.com/oklog/oklog/pkg/group"
 	opentracing "github.com/opentracing/opentracing-go"
@@ -28,8 +33,15 @@ func (a *App) defaultGRPCOptions(logger *zap.Logger, tracer opentracing.Tracer) 
 	options := []grpc.ServerOption{}
 
 	// UnaryInterceptor and OpenTracingServerInterceptor for tracer
-	options = append(options, grpc.UnaryInterceptor(
+	options = append(options, grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 		otgrpc.OpenTracingServerInterceptor(tracer, otgrpc.LogPayloads()),
+		grpc_auth.UnaryServerInterceptor(middleware.M.JWT),
+		grpc_prometheus.UnaryServerInterceptor,
+	),
+	))
+
+	options = append(options, grpc.StreamInterceptor(
+		grpc_auth.StreamServerInterceptor(middleware.M.JWT),
 	))
 
 	return options
