fdefb28dd4743c7745499c05e5690ede2cddca15
Emad <emadghaffariii@gmail.com>
2021-05-07 05:45:04 +0000

    exec from interface object

--- auth/main.go
+++ auth/main.go
@@ -8,7 +8,7 @@ import (
 
 // root execute command with cobra
 func main() {
-	if err := cmd.RootCmd().Execute(); err != nil {
+	if err := cmd.Runner.RootCmd().Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to run command: %v\n", err)
 		os.Exit(1)
 	}
