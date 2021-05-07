e9021f6f93db4036d110f4a41bbab43b8cb5583f
Emad <emadghaffariii@gmail.com>
2021-05-07 06:22:09 +0000

    add dao and dto files

--- auth/pkg/jtrace/jtrace_dto.go
+++ auth/pkg/jtrace/jtrace_dto.go
@@ -14,23 +14,6 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	tracer opentracing.Tracer
-	Tracer itracer = &jtracer{}
-)
-
-type itracer interface {
-	Connect() (io.Closer, error)
-	GetTracer() opentracing.Tracer
-	FromContext(ctx context.Context, startName string) opentracing.Span
-	StartSpan(str string) opentracing.Span
-	ContextWithSpan(ctx context.Context, span opentracing.Span) context.Context
-	SpanFromContext(ctx context.Context, name string, opts ...opentracing.StartSpanOption) (opentracing.Span, context.Context)
-	ChildOf(span opentracing.Span, name string) opentracing.Span
-}
-
-type jtracer struct{}
-
 // Connect method
 func (j *jtracer) Connect() (io.Closer, error) {
 	// Sample configuration for testing. Use constant sampling to sample every trace
