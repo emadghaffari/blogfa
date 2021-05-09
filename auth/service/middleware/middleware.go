8bc3ca4f5d3fd0a78dd2b2d869d13762dbe1cc6b
Emad <emadghaffariii@gmail.com>
2021-05-09 16:57:33 +0000

    comment

--- auth/service/middleware/middleware.go
+++ auth/service/middleware/middleware.go
@@ -11,6 +11,7 @@ type Middleware interface {
 	JWT(ctx context.Context) (context.Context, error)
 }
 
+// middle struct
 type middle struct{}
 
 func (m *middle) JWT(ctx context.Context) (context.Context, error) {
