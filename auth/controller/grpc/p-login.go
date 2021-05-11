6b998cff797fc9e464478ef7d8ae6d412907fe93
Emad Ghaffari <emadghaffariii@gmail.com>
2021-05-11 06:10:11 +0000

    remove logic from controller and send logics to service

--- auth/controller/grpc/p-login.go
+++ auth/controller/grpc/p-login.go
@@ -1,18 +1,11 @@
 package grpc
 
 import (
-	"blogfa/auth/client/broker"
-	"blogfa/auth/config"
-	"blogfa/auth/domain/jwt"
-	"blogfa/auth/domain/user"
 	"blogfa/auth/pkg/jtrace"
-	"blogfa/auth/pkg/token"
 	pb "blogfa/auth/proto"
+	"blogfa/auth/service/grpc"
 	"context"
 	"net/http"
-
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 // PLogin, login user with phone number with sms code
@@ -21,51 +14,17 @@ func (a *Auth) PLogin(ctx context.Context, req *pb.PLoginRequest) (*pb.PLoginRes
 	defer span.Finish()
 	span.SetTag("login", "phone login")
 
-	// get user with email or username
-	user, err := user.Model.Get(jtrace.Tracer.ContextWithSpan(ctx, span), "users", "phone = ? ", req.GetPhone())
+	response, err := grpc.Service.PLogin(jtrace.Tracer.ContextWithSpan(ctx, span), req)
 	if err != nil {
 		return &pb.PLoginResponse{
-			Message: "invalid phone number",
+			Message: "ERROR",
 			Status: &pb.Status{
+				Message: err.Error(),
 				Code:    http.StatusInternalServerError,
-				Message: "invalid phone number",
 			},
-		}, status.Errorf(codes.Internal, "invalid phone number")
+		}, err
 	}
-
-	// generate jwt token
-	jwt, err := jwt.Model.GenerateJWT()
-	if err != nil {
-		return &pb.PLoginResponse{
-			Message: "error in generate accessToken try after 10 seconds!",
-			Status: &pb.Status{
-				Code:    http.StatusInternalServerError,
-				Message: "error in generate accessToken try after 10 seconds!",
-			},
-		}, status.Errorf(codes.Internal, "error in generate accessToken try after 10 seconds!")
-	}
-
-	// make a map for jwt and user
-	data := make(map[string]interface{}, 2)
-	data["jwt"] = *jwt
-	data["user"] = user
-	notif := config.SMS{
-		Service: config.Global.Service.Name,
-		Token:   token.Generate(25),
-		Data:    data,
-		To:      user.Phone,
-	}
-
-	// publish to nats channel
-	broker.Nats.Publish(ctx, "service.notification.sms.auth", notif)
 
 	// return response for check the phone
-	return &pb.PLoginResponse{
-		Message: "check your phone!",
-		Token:   jwt.AccessUUID,
-		Status: &pb.Status{
-			Code:    http.StatusOK,
-			Message: "successfully",
-		},
-	}, nil
+	return response, nil
 }
